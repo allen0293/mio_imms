09807defd7f7e645222cc8559ddbcf9124c95183
allen0293 <allencandela0@gmail.com>
2026-07-11 14:55:24 +0800

    Milestone 1 - Laravel setup, authentication, dashboard foundation

---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\Http\Controllers\DashboardController;
+
+Route::middleware(['auth'])->group(function () {
+
+    Route::get('/dashboard', [DashboardController::class, 'index'])
+        ->name('dashboard');
+
+});
